25ddf02d221d9d383d171047b3e422b57800616e
Harrison <155126935+hmseaborn90@users.noreply.github.com>
2024-01-31 20:59:39 -0800

    updated review controller

--- src/main/java/com/harrison/movies/ReviewController.java
+++ src/main/java/com/harrison/movies/ReviewController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/movies")
+@RequestMapping("/api/v1/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
